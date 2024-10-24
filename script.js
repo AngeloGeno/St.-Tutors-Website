232d40e1040238d6d48f2269ab9d5fdd1805d3f4
AngeloGeno <76631482+AngeloGeno@users.noreply.github.com>
2024-10-24 23:47:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector("#menu-btn");
+let navbar =document.querySelector('#header .flex .navbar');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-graduation-cap');
+}
+
